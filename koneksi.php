1fb2d69a8fb924212bec91fed5d9fe5e8a4a73f0
syihwamoza <mozakastella26@gmail.com>
2026-01-17 16:40:55 +0700

    Mengatur charset utf8mb4 dan header API untuk respon JSON

--- koneksi.php
+++ koneksi.php
@@ -21,3 +21,13 @@ if ($conn->connect_error) {
         'message' => 'Koneksi database gagal: ' . $conn->connect_error
     ]));
 }
+// Set charset UTF-8
+$conn->set_charset('utf8mb4');
+
+// Header untuk JSON response
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+
+?>
